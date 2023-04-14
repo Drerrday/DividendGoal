03b5b8e2ffb914e9b0b151fb6e023575cd27dbea
Hunter <hunterelam21@gmail.com>
2023-04-13 22:24:52 -0500

    Added goal dividend and made it in OOP style

---
+++ DividendCalculator.py
@@ -0,0 +1,43 @@
+"""
+Created by Hunter Elam
+Python 3.9
+IDE: IntelliJ
+Dec. 17, 2020
+"""
+
+def dividend_calculator(stock_price, div_yield, shares, goal_dividend):
+    # Real Action
+    investment = stock_price * shares
+    annual_div = stock_price * div_yield
+    qtr_div = annual_div / 4
+
+    # Calculate the number of shares needed for the goal dividend
+    shares_needed = goal_dividend / annual_div
+
+    # Formatting
+    print("*" * 50)
+    print("At ${:.2f} per share".format(stock_price))
+    print("You have ${:.2f} invested into this particular company.".format(investment))
+    print()
+    print("Quarterly Payouts (3-months)")
+    print("Quarterly Dividend PER SHARE: {:.2f} cents".format(qtr_div))
+    print("Quarterly Dividend ALL SHARES: ${:.2f}".format(qtr_div * shares))
+    print()
+    print("Yearly Payouts (12-months)")
+    print("Annual Dividend PER SHARE: {:.2f} cents".format(annual_div))
+    print("Annual Dividend ALL SHARES: ${:.2f}".format(annual_div * shares))
+    print()
+    print("Goal dividend: ${:.2f}".format(goal_dividend))
+    print("To reach the goal dividend, you would need to own approximately {:.0f} shares.".format(shares_needed))
+    print("{} more shares at the current stock price of ${:.2f} would mean you would have to invest an additional ${:.2f}".format(int(shares_needed) - shares, stock_price, (int(shares_needed) - shares) * stock_price))
+    print("*" * 50)
+
+# Collect user input
+stock_price = float(input("Please enter the current stock price: "))
+div_yield = float(input("Please enter the current dividend yield (as a percentage): ")) / 100
+shares = int(input("Please enter the amount of shares owned: "))
+goal_dividend = float(input("Please enter your goal dividend: "))
+
+# Call the function with user input
+dividend_calculator(stock_price, div_yield, shares, goal_dividend)
+input()
